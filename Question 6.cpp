1df375c0c809582f6adbed08cf781881ede0a8ab
HaythamMetawie <haytham.h3@gmail.com>
2015-10-19 21:24:26 +0200

    Update Question 6.cpp

--- Question 6.cpp
+++ Question 6.cpp
@@ -2,17 +2,19 @@
 using namespace std;
 class Move
 {
-   private:
-	   double x; double y;
-   public:
-	void Move(double a = 0, double b = 0); // sets x, y to a, b
+private:
+	double x; double y;
+public:
+	void move(double a = 0, double b = 0); // sets x, y to a, b
 	void showmove() const; // shows current x, y values 
-    void Move add(const Move & m) const; //this function adds x of m to x of invoking object to get new x,
+	void  Move add(const Move & m) const; //this function adds x of m to x of invoking object to get new x,
 	//adds y of m to y of invoking object to get new y, creates a new
 	//move object initialized to new x, y values and returns it
-	void reset(double a = 0, double b = 0); // resets x,y to a, b
+	void reset(double a = 0 , double b = 0); // resets x,y to a, b
+	double get_x() const;
+	double get_y() const;
 };
-void Move::Move(double a = 0 , double b = 0)
+void Move::move(double a = 0, double b = 0)
 {
 	double x = a; double y = b;
 }
@@ -21,22 +23,22 @@ void Move::reset(double a = 0, double b = 0)
 	double x = a; double y = b;
 }
 void Move::showmove() const
- {
-	 cout << "(" << x << "," << y << ")" << endl;
- }
-double get_x() const
+{
+	cout << "(" << x << "," << y << ")" << endl;
+}
+double Move::get_x() const
 {
 	return x;
 }
-double get_y() const
+double Move::get_y() const
 {
 	return y;
 }
-void Move::add(const Move &m) const
+void Move::Move add(const Move &m) const
 {
-	Move t;
-	double x1 = t.get_x() const;
-	double y1 = t.get_y() const;
+	Move m;
+	double x1 = mt.get_x() const;
+	double y1 = m.get_y() const;
 	double x2 = x + x1;
 	double y2 = y + y1;
 }
