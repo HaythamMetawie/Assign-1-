2a5a11a641c7e619617e1d4f6fe794099321bb2a
HaythamMetawie <haytham.h3@gmail.com>
2015-10-19 22:16:32 +0200

    Update Question 6.cpp

--- Question 6.cpp
+++ Question 6.cpp
@@ -5,16 +5,16 @@ class Move
 private:
 	double x; double y;
 public:
-	void move(double a = 0, double b = 0); // sets x, y to a, b
+	Move(double a = 0, double b = 0); // sets x, y to a, b
 	void showmove() const; // shows current x, y values 
-	void  Move add(const Move & m) const; //this function adds x of m to x of invoking object to get new x,
+    Move add(const Move & m) const; //this function adds x of m to x of invoking object to get new x,
 	//adds y of m to y of invoking object to get new y, creates a new
 	//move object initialized to new x, y values and returns it
 	void reset(double a = 0 , double b = 0); // resets x,y to a, b
 	double get_x() const;
 	double get_y() const;
 };
-void Move::move(double a = 0, double b = 0)
+ Move::Move(double a = 0, double b = 0)
 {
 	double x = a; double y = b;
 }
@@ -34,13 +34,16 @@ double Move::get_y() const
 {
 	return y;
 }
-void Move::Move add(const Move &m) const
+ Move Move::add(const Move &m) const
 {
-	Move m;
-	double x1 = mt.get_x() const;
-	double y1 = m.get_y() const;
-	double x2 = x + x1;
-	double y2 = y + y1;
+	Move t = m;
+	double x1, x2, y1, y2;
+	 x1 = t.get_x();
+	 y1 = t.get_y();
+	 x2 = x + x1;
+	 y2 = y + y1;
+	 Move t(x2,y2);
+		 return t;
 }
 
 int main()
@@ -48,4 +51,5 @@ int main()
 	Move a(1, 2);
 	Move b(3, 4);
 	Move c = a.add(b);
+	c.showmove();
 }
