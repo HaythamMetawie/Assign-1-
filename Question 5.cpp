f276aa1d1d52b16915fa6148e8012013955b959d
HaythamMetawie <haytham.h3@gmail.com>
2015-10-19 18:42:32 +0200

    Update Question 5.cpp

--- Question 5.cpp
+++ Question 5.cpp
@@ -6,30 +6,49 @@ class Bank_Account
 private:
 	string client_name;
 	string account_number;
-public:
 	double balance;
-	Bank_Account(string client_name = "Haytham Tareq", string account_number = "33854", double balance = 10000.0);
+public:
+	Bank_Account(string client_name , double balance);
+	Account_Number(string number);
 	void account_information();
 	double deposite(double d);
 	double withdraw(double w);
 };
-void account_information()
+void Bank_Account::Bank_Account(string client_name, double b)
+{ 
+   string client_name;
+   cin >> client_name;
+   b= 10000.0;
+}
+void Bank_Account::Account_Number(string number) { string number = "33854";}
+void Bank_Account::account_information()
 {
-	cout << client_name << endl << account_number << endl;
+	cout << "Client Name :" << client_name << endl;
+	cout << "Account Number :" << account_number << endl;
+	cout << "Your Blanace :" << balance << endl;
 }
 double Bank_Account::deposite(double d)
 {
-	double balance = balance + d;
+        balance+= d;
 	return balance;
 }
 double Bank_Account::withdraw(double w)
-{
-	double balance = balance - w;
-	return balance;
+{ 
+	if (balance < w)
+	{ cout << "Sorry the account balance does not cover the withdrawal!" << endl;}
+	else 
+        {balance-= w;
+	return balance;}
 }
 int main()
 {
+	cout << "Enter your Name :" << endl;
 	Bank_Account X;
+	cout << "Enter your deposite amount :" <<endl;
+	X.deposite (double d);
+	cout << "Enter your withdrawal amount :" << endl;
+	X.withdraw (double w);
 	X.account_information();
-	cout << X.deposite(1000.0) << endl << X.withdraw(500.0) << endl;
+	
+	
 }
